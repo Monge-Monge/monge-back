f8495d73af6ea9ad58ac144e2b04920a90c3cda3
wsdf25867 <wsdf25867@gmail.com>
2024-07-04 06:59:12 +0900

    feat : initial commit

---
+++ src/main/kotlin/com/monge_monge/MongeMongeApplication.kt
@@ -0,0 +1,11 @@
+package com.monge_monge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MongeMongeApplication
+
+fun main(args: Array<String>) {
+	runApplication<MongeMongeApplication>(*args)
+}
---
+++ src/test/kotlin/com/monge_monge/MongeMongeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.monge_monge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MongeMongeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
